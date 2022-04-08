ddefb18c3356f997ade540c44a31c20285386bf1
vino <vino0908@outlook.com>
2022-04-09 00:18:33 +0800

    feat: 添加样式

--- src/lib/Button/src/Button.tsx
+++ src/lib/Button/src/Button.tsx
@@ -1,10 +1,19 @@
-import { defineComponent } from "vue";
+import { computed, defineComponent, type PropType } from "vue";
 import s from "./style/button.module.css";
 
 export const Button = defineComponent({
+  props: {
+    type: {
+      type: String as PropType<"primary" | "default" | "danger">,
+      default: "default",
+    },
+  },
   setup(props, { slots }) {
+    const classes = computed(() => {
+      return [s.layout, s[props.type]];
+    });
     return () => (
-      <button class={s.layout}>
+      <button class={classes.value}>
         <span>{slots.default?.()}</span>
       </button>
     );
