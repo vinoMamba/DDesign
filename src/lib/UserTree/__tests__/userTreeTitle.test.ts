33e31459babf78ea5d4ab86a7d896ef1f18b2be3
vino <vino0908@outlook.com>
2022-04-11 13:40:57 +0800

    test: add userTreeTitle.test

---
+++ src/lib/UserTree/__tests__/userTreeTitle.test.ts
@@ -0,0 +1,16 @@
+import {expect, test} from "vitest";
+import {mount} from "@vue/test-utils";
+import {UserTreeTitle} from "../components/UserTreeTitle";
+
+test("the title prop have default value", () => {
+    const wrapper = mount(UserTreeTitle, {});
+    expect(wrapper.text()).toEqual("选择人员");
+})
+test("can set title prop", () => {
+    const wrapper = mount(UserTreeTitle, {
+        propsData: {
+            title: "test"
+        }
+    });
+    expect(wrapper.text()).toEqual("test");
+})
