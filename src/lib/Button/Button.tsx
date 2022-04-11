3f10f09c848c6ea07d674250bd496c87eb72fccb
vino <vino0908@outlook.com>
2022-04-11 08:19:26 +0800

    feat: button组件添加disabled属性

--- src/lib/Button/Button.tsx
+++ src/lib/Button/Button.tsx
@@ -7,14 +7,19 @@ const Button = defineComponent({
       type: String as PropType<"primary" | "default" | "danger">,
       default: "default",
     },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
   },
   emits: ["click"],
   setup(props, { slots, emit }) {
     const classes = computed(() => {
-      return [s.layout, s[props.type]];
+      return [s.layout, s[props.type], s[props.disabled ? "disabled" : ""]];
     });
     return () => (
       <button
+        disabled={props.disabled}
         class={classes.value}
         onClick={(e: MouseEvent) => emit("click", e)}
       >
--- src/lib/Button/__tests__/Button.test.ts
+++ src/lib/Button/__tests__/Button.test.ts
@@ -14,3 +14,14 @@ test("HButton", () => {
   expect(wrapper.text()).toBe("click me");
   expect(wrapper.classes()).toMatch(/primary/);
 });
+
+test('can set "disabled" prop', async () => {
+  const wrapper = mount(HButton, {
+    propsData: {
+      disabled: true,
+    },
+  });
+  expect(wrapper.classes()).toMatch(/disabled/);
+  expect(wrapper.find("button").element.disabled).toBe(true);
+  expect(await wrapper.find("button").trigger("click")).toBeFalsy();
+});
