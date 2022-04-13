376d87fade580c0b0b05f1d279eff7397540c9c7
vino <vino0908@outlook.com>
2022-04-13 22:28:02 +0800

    feat: 解决vue3 watch懒监听导致监听不到数据变化的问题

--- src/lib/UserTree/UserTreeContent.tsx
+++ src/lib/UserTree/UserTreeContent.tsx
@@ -1,34 +1,46 @@
-import {defineComponent, ref, unref} from "vue";
+import { defineComponent, ref, unref } from "vue";
 import s from "./style/userTreeContent.module.css";
-import {HInput} from "@/lib";
-import {UserTreeNodeList} from "./components/UserTreeNodeList";
-import type {TreeNode} from "./type";
-import {mockTree} from "./mock";
-import {UserTreeOperationList} from "@/lib/UserTree/components/UserTreeOperationList";
+import { HInput } from "@/lib";
+import {
+  UserTreeNodeList,
+  type UniqueTreeNode,
+} from "./components/UserTreeNodeList";
+import type { TreeNode } from "./type";
+import { mockTree } from "./mock";
+import { UserTreeOperationList } from "@/lib/UserTree/components/UserTreeOperationList";
 
 export const UserTreeContent = defineComponent({
-    name: "UserTreeContent",
-    setup() {
-        const searchContent = ref("");
-        const listRef = ref<TreeNode[]>(mockTree);
-        const checkedNodes = ref<TreeNode[]>([]);
-        const deleteNode = ref<TreeNode>()
-
-        return () => (
-            <div class={s.layout}>
-                <div class={s.left}>
-                    <HInput v-model:value={searchContent.value} placeholder="搜索"/>
-                    <UserTreeNodeList list={listRef.value} v-model:checkedNodes={checkedNodes.value}
-                                      deleteNode={unref(deleteNode)}/>
-                </div>
-                <div class={s.right}>
-                    <div class={s.content}>
-                        <UserTreeOperationList checkedNodes={checkedNodes.value} onDelete={(node) => {
-                            deleteNode.value = node
-                        }}/>
-                    </div>
-                </div>
-            </div>
-        );
-    },
+  name: "UserTreeContent",
+  setup() {
+    const searchContent = ref("");
+    const listRef = ref<TreeNode[]>(mockTree);
+    const checkedNodes = ref<TreeNode[]>([]);
+    const deleteNode = ref<UniqueTreeNode>();
+    function createUniqueNode(node: TreeNode) {
+      deleteNode.value = {
+        ...node,
+        uniqueId: Symbol(),
+      };
+    }
+    return () => (
+      <div class={s.layout}>
+        <div class={s.left}>
+          <HInput v-model:value={searchContent.value} placeholder="搜索" />
+          <UserTreeNodeList
+            list={listRef.value}
+            v-model:checkedNodes={checkedNodes.value}
+            deleteNode={deleteNode.value}
+          />
+        </div>
+        <div class={s.right}>
+          <div class={s.content}>
+            <UserTreeOperationList
+              checkedNodes={checkedNodes.value}
+              onDelete={(node) => createUniqueNode(node)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  },
 });
--- src/lib/UserTree/components/UserTreeNodeList.tsx
+++ src/lib/UserTree/components/UserTreeNodeList.tsx
@@ -1,125 +1,164 @@
-import {computed, defineComponent, inject, onMounted, type PropType,  ref, watch, watchEffect} from "vue";
-import type {TreeNode} from "../type";
-import s from "./style/userTreeNodeList.module.css"
-import {type UserTreeInjection, userTreeInjection} from "@/lib/UserTree/UserTree";
-import {getCheckedNodes, traverseNodeList, traverseTree} from "@/lib/UserTree/utils";
+import {
+  computed,
+  defineComponent,
+  inject,
+  onMounted,
+  type PropType,
+  ref,
+  watch,
+} from "vue";
+import type { TreeNode } from "../type";
+import s from "./style/userTreeNodeList.module.css";
+import {
+  type UserTreeInjection,
+  userTreeInjection,
+} from "@/lib/UserTree/UserTree";
+import {
+  getCheckedNodes,
+  traverseNodeList,
+  traverseTree,
+} from "@/lib/UserTree/utils";
 
-
+// 由于vue3 里面的watch 是懒监听，必须加一个symbol值保证唯一
+export interface UniqueTreeNode extends TreeNode {
+  uniqueId: Symbol;
+}
 export const UserTreeNodeList = defineComponent({
-        name: "UserTreeNodeList",
-        props: {
-            list: {
-                type: Array as PropType<TreeNode[]>,
-                default: () => [],
-            },
-            deleteNode: {
-                type: Object as PropType<TreeNode>,
-            },
-        },
-        emits: ["update:checkedNodes"],
-        setup(props, {emit}) {
-            const UserTree = inject(userTreeInjection, {
-                mode: () => 'department',
-                multiple: () => true,
-                showUserCount: () => true
-            } as UserTreeInjection);
-            const userCountVisible = computed(() => {
-                return UserTree.showUserCount() && UserTree.mode() === 'andUser';
-            })
-            const breadcrumbList = ref<TreeNode[]>([]);
-            const nodeList = ref<TreeNode[]>([]);
-            const reset = () => {
-                breadcrumbList.value = [];
-                updateNodeList(props.list, {mode: UserTree.mode()});
-            }
-            const getNext = (node: TreeNode) => {
-                if (node.type === 1) return
-                breadcrumbList.value.push(node);
-                updateNodeList(node.children, {mode: UserTree.mode()});
-            };
-            const getPrev = (node: TreeNode) => {
-                const index = breadcrumbList.value.findIndex(n => n.id === node.id);
-                breadcrumbList.value = breadcrumbList.value.slice(0, index + 1);
-                updateNodeList(node.children, {mode: UserTree.mode()});
-            }
+  name: "UserTreeNodeList",
+  props: {
+    list: {
+      type: Array as PropType<TreeNode[]>,
+      default: () => [],
+    },
+    deleteNode: {
+      // TreeNode | undefined
+      type: Object as PropType<UniqueTreeNode>,
+    },
+  },
+  emits: ["update:checkedNodes"],
+  setup(props, { emit }) {
+    const UserTree = inject(userTreeInjection, {
+      mode: () => "department",
+      multiple: () => true,
+      showUserCount: () => true,
+    } as UserTreeInjection);
+    const userCountVisible = computed(() => {
+      return UserTree.showUserCount() && UserTree.mode() === "andUser";
+    });
+    const breadcrumbList = ref<TreeNode[]>([]);
+    const nodeList = ref<TreeNode[]>([]);
+    const reset = () => {
+      breadcrumbList.value = [];
+      updateNodeList(props.list, { mode: UserTree.mode() });
+    };
+    const getNext = (node: TreeNode) => {
+      if (node.type === 1) return;
+      breadcrumbList.value.push(node);
+      updateNodeList(node.children, { mode: UserTree.mode() });
+    };
+    const getPrev = (node: TreeNode) => {
+      const index = breadcrumbList.value.findIndex((n) => n.id === node.id);
+      breadcrumbList.value = breadcrumbList.value.slice(0, index + 1);
+      updateNodeList(node.children, { mode: UserTree.mode() });
+    };
 
-            function updateNodeList(data: TreeNode[], options: { mode: 'andUser' | 'department' }) {
-                nodeList.value = data;
-                const {mode} = options;
-                if (mode === 'department') {
-                    nodeList.value = nodeList.value.filter(node => node.type === 0)
-                }
-            }
+    function updateNodeList(
+      data: TreeNode[],
+      options: { mode: "andUser" | "department" }
+    ) {
+      nodeList.value = data;
+      const { mode } = options;
+      if (mode === "department") {
+        nodeList.value = nodeList.value.filter((node) => node.type === 0);
+      }
+    }
 
+    function toggleChecked(e: Event, node: TreeNode) {
+      node.checked = !node.checked;
+      // change children checked state
+      traverseTree(node, (n: TreeNode) => {
+        n.checked = node.checked;
+      });
+      // change parent checked state
+      updateParentCheckedState();
+      updateCheckedNodes();
+    }
+    function updateParentCheckedState() {
+      const lastNode = breadcrumbList.value[breadcrumbList.value.length - 1];
+      if (lastNode && lastNode.children.every((n) => n.checked)) {
+        lastNode.checked = true;
+      } else if (lastNode && !lastNode.children.some((n) => n.checked)) {
+        lastNode.checked = false;
+      }
+    }
 
-            function toggleChecked(e: Event, node: TreeNode) {
-                node.checked = !node.checked;
-                // change children checked state
-                traverseTree(node, (n: TreeNode) => {
-                    n.checked = node.checked;
-                })
-                // change parent checked state
-                const lastNode = breadcrumbList.value[breadcrumbList.value.length - 1];
-                if (lastNode && lastNode.children.every(n => n.checked)) {
-                    lastNode.checked = true;
-                } else if (lastNode && !lastNode.children.some(n => n.checked)) {
-                    lastNode.checked = false;
-                }
-                updateCheckedNodes()
-            }
+    function updateCheckedNodes() {
+      const checkedNodes = getCheckedNodes(nodeList.value);
+      emit("update:checkedNodes", checkedNodes);
+    }
 
-            function updateCheckedNodes() {
-                const checkedNodes = getCheckedNodes(nodeList.value)
-                emit('update:checkedNodes', checkedNodes)
-            }
-
-            //watch deleteNode and changed checked state
-            watch(() => props.deleteNode, (newVal) => {
-                //traverse  nodeList to find node
-                traverseNodeList(nodeList.value, (node: TreeNode) => {
-                    if (node.id === newVal!.id) {
-                        node.checked = false;
-                        updateCheckedNodes()
-                    }
-                })
-            })
-            onMounted(() => {
-                updateNodeList(props.list, {mode: UserTree.mode()});
-            });
-            return () => (
-                <div>
-                    <ol class={s.breadcrumb}>
-                        <li onClick={() => reset()}>
-                            <span class={s.title}>通讯录</span>
-                            <span class={s.separator}>&gt;</span>
-                        </li>
-                        {breadcrumbList.value.map((node, index) => (
-                            <li title={node.name} key={node.id} onClick={() => getPrev(node)}>
-                                <span class={s.title}>{node.name}</span>
-                                {index < breadcrumbList.value.length - 1 && <span class={s.separator}>&gt;</span>}
-                            </li>))}
-                    </ol>
-                    <ul class={s.list}>
-                        {nodeList.value.length === 0 ? (
-                            <li class={s.empty}>暂无数据</li>
-                        ) : (
-                            nodeList.value.map((node) => (
-                                <li key={node.id}>
-                                    <input type="checkbox" class={s.checkbox} checked={node.checked}
-                                           onChange={(e: Event) => toggleChecked(e, node)}/>
-                                    <p class={s.node} onClick={() => getNext(node)}>
-                                        <img src={node.avatar} alt=''/>
-                                        <span class={s.name}>{node.name}</span>
-                                        {(node.type === 0 && userCountVisible.value) ?
-                                            <span class={s.count}>（{node.peopleCount}）</span> : null}
-                                    </p>
-                                    {node.type === 0 && <span class={s.next} onClick={() => getNext(node)}>下级</span>}
-                                </li>
-                            ))
-                        )}
-                    </ul>
-                </div>
-            );
-        },
-    })
-;
+    //watch deleteNode and changed checked state
+    watch(
+      () => props.deleteNode,
+      (newVal) => {
+        //traverse  nodeList to find node
+        traverseNodeList(nodeList.value, (node: TreeNode) => {
+          if (node.id === newVal!.id) {
+            node.checked = false;
+            updateParentCheckedState();
+            updateCheckedNodes();
+          }
+        });
+      }
+    );
+    onMounted(() => {
+      updateNodeList(props.list, { mode: UserTree.mode() });
+    });
+    return () => (
+      <div>
+        <ol class={s.breadcrumb}>
+          <li onClick={() => reset()}>
+            <span class={s.title}>通讯录</span>
+            <span class={s.separator}>&gt;</span>
+          </li>
+          {breadcrumbList.value.map((node, index) => (
+            <li title={node.name} key={node.id} onClick={() => getPrev(node)}>
+              <span class={s.title}>{node.name}</span>
+              {index < breadcrumbList.value.length - 1 && (
+                <span class={s.separator}>&gt;</span>
+              )}
+            </li>
+          ))}
+        </ol>
+        <ul class={s.list}>
+          {nodeList.value.length === 0 ? (
+            <li class={s.empty}>暂无数据</li>
+          ) : (
+            nodeList.value.map((node) => (
+              <li key={node.id}>
+                <input
+                  type="checkbox"
+                  class={s.checkbox}
+                  checked={node.checked}
+                  onChange={(e: Event) => toggleChecked(e, node)}
+                />
+                <p class={s.node} onClick={() => getNext(node)}>
+                  <img src={node.avatar} alt="" />
+                  <span class={s.name}>{node.name}</span>
+                  {node.type === 0 && userCountVisible.value ? (
+                    <span class={s.count}>（{node.peopleCount}）</span>
+                  ) : null}
+                </p>
+                {node.type === 0 && (
+                  <span class={s.next} onClick={() => getNext(node)}>
+                    下级
+                  </span>
+                )}
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
+    );
+  },
+});
